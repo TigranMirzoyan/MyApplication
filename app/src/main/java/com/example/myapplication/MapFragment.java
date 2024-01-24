51279bd830c6017eba6196e1c55f9150c6128c0e
Tigran <tigmir5@gmail.com>
2024-01-24 13:05:35 +0400

    commit

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -1,30 +1,33 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.google.android.gms.maps.CameraUpdate;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapFragment extends Fragment {
 
+    private GoogleMap googleMap;
+    private List<Marker> markerList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
 
         // инициализировать View
         View view = inflater.inflate(R.layout.fragment_map, container, false);
@@ -34,19 +37,27 @@ public class MapFragment extends Fragment {
 
         // async map (выполнение задачи без ожидания завершения других задач. Это позволяет программе выполнять другие операции, не блокируя выполнение)
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-
-
             @Override
-            public void onMapReady(@NonNull GoogleMap googleMap) {
+            public void onMapReady(@NonNull GoogleMap map) {
+                googleMap = map;
 
                 Button button1 = view.findViewById(R.id.button1);
+                Button button2 = view.findViewById(R.id.button2);
                 Button button3 = view.findViewById(R.id.button3);
 
                 // удалить все маркеры
                 button1.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        googleMap.clear();
+                        clearMarkers();
+                    }
+                });
+
+                // удалить последний маркеры
+                button2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        deleteLastMarker();
                     }
                 });
 
@@ -54,39 +65,43 @@ public class MapFragment extends Fragment {
                 button3.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // когда карта загружена
-                        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                            @Override
-                            public void onMapClick(@NonNull LatLng latLng) {
-                                // при нажатии на карту инициализируются параметры маркера
-                                MarkerOptions markerOptions = new MarkerOptions();
-
-                                // установить положение маркера
-                                markerOptions.position(latLng);
-
-                                // установить название маркера
-                                markerOptions.title(latLng.latitude + ":" + latLng.longitude);
-
-                                // анимация масштабирования маркера
-                                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-
-                                // добавить маркер на карту
-                                googleMap.addMarker(markerOptions);
-
-                                // убрать обработчик события после добавления маркера
-                                googleMap.setOnMapClickListener(null);
-                            }
-                        });
+                        addMarker();
                     }
                 });
-
-                // увеличить и уменьшить
-                googleMap.getUiSettings().setZoomControlsEnabled(true);
-                googleMap.getUiSettings().setCompassEnabled(true);
-
-
             }
         });
+
         return view;
+    }
+
+    private void clearMarkers() {
+        googleMap.clear();
+        markerList.clear();
+    }
+
+    private void deleteLastMarker() {
+        if (!markerList.isEmpty()) {
+            Marker lastMarker = markerList.remove(markerList.size() - 1);
+            lastMarker.remove();
+        }
+    }
+
+    private void addMarker() {
+        if (googleMap != null) {
+            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                @Override
+                public void onMapClick(@NonNull LatLng latLng) {
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .position(latLng)
+                            .title(latLng.latitude + ":" + latLng.longitude);
+
+                    Marker marker = googleMap.addMarker(markerOptions);
+                    markerList.add(marker);
+
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                    googleMap.setOnMapClickListener(null);
+                }
+            });
+        }
     }
 }
