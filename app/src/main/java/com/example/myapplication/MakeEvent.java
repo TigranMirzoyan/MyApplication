77debc8c1647bec5d0c8c26f1f89237dad07cae6
Tigran <tigmir5@gmail.com>
2024-02-02 23:45:32 +0400

    commit

---
+++ app/src/main/java/com/example/myapplication/MakeEvent.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
+
+public class MakeEvent extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_make_event, container, false);
+
+        Button button1 = view.findViewById(R.id.button1);
+        DatePicker datePicker = view.findViewById(R.id.datePicker);
+
+        // Установите текущую дату в DatePicker
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+        datePicker.init(year, month, dayOfMonth, null);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+        return view;
+    }
+}
