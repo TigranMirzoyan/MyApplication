3dabf2d815041caeee924b069c5bdfd64f2bf539
Tigran <tigmir5@gmail.com>
2024-02-20 22:05:57 +0400

    Global changes and additions

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -4,8 +4,6 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,49 +14,38 @@ import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-
-import androidx.core.content.ContextCompat;
-
 import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-import androidx.appcompat.widget.SearchView;
-
-
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MapFragment extends Fragment {
-    private GoogleMap googleMap;
+    private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private int buttonVisBool = 0;
     private AutocompleteSupportFragment autocompleteFragment;
     Marker clickedMarker = null;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private boolean isUserLocationVisible = false;
+
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // инициализировать View
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
@@ -83,47 +70,54 @@ public class MapFragment extends Fragment {
             public void onPlaceSelected(@NonNull Place place) {
                 LatLng latLng = place.getLatLng();
                 if (latLng != null) {
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
                 }
             }
 
             @Override
             public void onError(@NonNull Status status) {
-                Log.i("MapFragment", "An error occurred: " + status);
+                Log.i("MapFragment", " " + status);
             }
         });
 
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(@NonNull GoogleMap map) {
-                googleMap = map;
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                mMap = googleMap;
                 Button deleteButton = view.findViewById(R.id.button1);
                 Button addButton = view.findViewById(R.id.button2);
                 deleteButton.setVisibility(View.INVISIBLE);
+                View view = getView();
 
 
-                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted, request it
-                    ActivityCompat.requestPermissions(requireActivity(),
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                            LOCATION_PERMISSION_REQUEST_CODE);
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
                 } else {
-                    // Permission is granted, enable the My Location button
-                    googleMap.setMyLocationEnabled(true);
                 }
-                googleMap.setMyLocationEnabled(true);
 
-                googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                if (view != null) {
+                    view.findViewById(R.id.MyLocationbtn).setOnClickListener(v -> moveToCurrentLocation());
+                }
+
+                mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+                    @Override
+                    public void onCameraIdle() {
+                        checkIfUserLocationVisibleAndUpdateButton();
+                    }
+                });
+
+                mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                     @Override
                     public boolean onMarkerClick(Marker marker) {
                         if (clickedMarker == null || !clickedMarker.equals(marker)) {
                             deleteButton.setVisibility(View.VISIBLE);
 
-                            // Get the screen coordinates of the marker
-                            Point markerPoint = googleMap.getProjection().toScreenLocation(marker.getPosition());
+                            // получить кординаты маркера
+                            Point markerPoint = mMap.getProjection().toScreenLocation(marker.getPosition());
 
-                            // Adjust the position of the button to be above the marker
+                            // deletebuttun переместится на маркер
                             RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) deleteButton.getLayoutParams();
                             layoutParams.leftMargin = markerPoint.x - (deleteButton.getWidth() / 2);
                             layoutParams.topMargin = markerPoint.y - deleteButton.getHeight() - 95;
@@ -137,7 +131,6 @@ public class MapFragment extends Fragment {
                         return true;
                     }
                 });
-
 
                 deleteButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -157,17 +150,15 @@ public class MapFragment extends Fragment {
                     }
                 });
             }
-
         });
 
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationProviderClient.getLastLocation()
                     .addOnSuccessListener(requireActivity(), location -> {
                         if (location != null) {
                             LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            if (googleMap != null) {
-                                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+                            if (mMap != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
                             }
                         }
                     });
@@ -179,26 +170,56 @@ public class MapFragment extends Fragment {
         return view;
     }
 
-
-        private void clearMarkers() {
-        googleMap.clear();
-    }
-
     private void addMarker() {
-        if (googleMap != null) {
-            googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        if (mMap != null) {
+            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                 @Override
                 public void onMapClick(@NonNull LatLng latLng) {
                     MarkerOptions markerOptions = new MarkerOptions()
                             .position(latLng)
                             .title(latLng.latitude + ":" + latLng.longitude);
 
-                    Marker marker = googleMap.addMarker(markerOptions);
-
-                    googleMap.setOnMapClickListener(null);
+                    Marker marker = mMap.addMarker(markerOptions);
+                    mMap.setOnMapClickListener(null);
                 }
             });
         }
     }
-}
 
+    private void moveToCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(requireActivity(), location -> {
+                    if (location != null) {
+                        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+                    }
+                });
+    }
+
+    private void checkIfUserLocationVisibleAndUpdateButton() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(requireActivity(), location -> {
+            if (location != null && mMap != null) {
+                LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                isUserLocationVisible = mMap.getProjection().getVisibleRegion().latLngBounds.contains(currentLatLng);
+
+                Button myLocationButton = getView().findViewById(R.id.MyLocationbtn);
+                if (isUserLocationVisible) {
+                    myLocationButton.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.my_location_visible));
+                } else {
+                    myLocationButton.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.my_location_not_visible));
+                }
+            }
+        });
+    }
+}
