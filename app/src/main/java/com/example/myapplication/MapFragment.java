e87bb4c0cdc187b8648679581073a708858f2fb9
Tigran <tigmir5@gmail.com>
2024-02-22 15:21:56 +0400

    comment

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -35,9 +35,7 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 
 import java.util.Arrays;
 
-// Определение класса Fragment для работы с Google Maps и автокомплитом местоположений
 public class MapFragment extends Fragment {
-    // Определение основных переменных для работы с картой, местоположением и UI элементами
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private AutocompleteSupportFragment autocompleteFragment;
@@ -57,11 +55,6 @@ public class MapFragment extends Fragment {
         // Инициализация и настройка фрагмента карты
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
-        // Инициализация и настройка фрагмента автокомплита для поиска мест
-        if (!Places.isInitialized()) {
-            // Инициализация Places API с ключом API
-            Places.initialize(requireActivity().getApplicationContext(), "AIzaSyCCmIaUzr43cDsJmXee0li1d1aoq9SffKQ");
-        }
         autocompleteFragment = (AutocompleteSupportFragment) getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
         autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
 
@@ -75,26 +68,49 @@ public class MapFragment extends Fragment {
                     mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
                 }
             }
-
             @Override
             public void onError(@NonNull Status status) {
-                // Обработка ошибок при выборе места
-                Log.i("MapFragment", "An error occurred: " + status);
+                Log.i("MapFragment", " " + status); // Обработка ошибок при выборе места
             }
         });
+
+        // Проверка разрешений на доступ к местоположению при создании фрагмента
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // Запрашиваем последнее известное местоположение
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(requireActivity(), location -> {
+                        // Проверяем, получено ли местоположение
+                        if (location != null) {
+                            // Создаем объект LatLng с координатами местоположения
+                            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            // Если объект карты не null, перемещаем камеру к текущему местоположению
+                            if (mMap != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
+                            }
+                        }
+                    });
+        } else {
+            // Если разрешение не получено, запрашиваем его у пользователя
+            ActivityCompat.requestPermissions(requireActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+        // Инициализация и настройка фрагмента автокомплита для поиска мест
+        if (!Places.isInitialized()) {
+            Places.initialize(requireActivity().getApplicationContext(), "AIzaSyCCmIaUzr43cDsJmXee0li1d1aoq9SffKQ"); // Инициализация Places API с ключом API
+        }
 
         // Когда карта готова, инициализируем её и настраиваем UI элементы
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
-                mMap = googleMap; // Сохранение ссылки на GoogleMap
-
-                // Находим кнопки
+                mMap = googleMap;
                 Button deleteButton = view.findViewById(R.id.button1);
                 Button addButton = view.findViewById(R.id.button2);
                 deleteButton.setVisibility(View.INVISIBLE);
 
-                // Пытаемся найти компас на карте и перенастраиваем его положение
+                // находим компас на карте и перенастраиваем его положение
                 View view = getView();
                 if (view != null && view.findViewById(Integer.parseInt("1")) != null) {
                     View locationCompass = ((View) view.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("5"));
@@ -105,8 +121,7 @@ public class MapFragment extends Fragment {
                     layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
                     layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
 
-                    // Задаем отступы для компаса
-                    layoutParams.setMargins(30, 0, 0, 100);
+                    layoutParams.setMargins(30, 0, 0, 100); // Задаем отступы для компаса
                     locationCompass.setLayoutParams(layoutParams);
                 }
 
@@ -123,9 +138,9 @@ public class MapFragment extends Fragment {
                 }
 
                 // Устанавливаем слушателя на изменение позиции камеры для обновления UI в зависимости от видимости местоположения пользователя
-                mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+                mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
                     @Override
-                    public void onCameraIdle() {
+                    public void onCameraMove() {
                         checkIfUserLocationVisibleAndUpdateButton();
                     }
                 });
@@ -175,37 +190,14 @@ public class MapFragment extends Fragment {
                         addMarker(); // Вызываем метод добавления маркера на карту
                     }
                 });
-            } // Конец блока onMapReady
+            }
         });
-
-        // Проверка разрешений на доступ к местоположению при создании фрагмента
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Если разрешение получено, запрашиваем последнее известное местоположение
-            fusedLocationProviderClient.getLastLocation()
-                    .addOnSuccessListener(requireActivity(), location -> {
-                        // Проверяем, получено ли местоположение
-                        if (location != null) {
-                            // Создаем объект LatLng с координатами местоположения
-                            LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            // Если объект карты не null, перемещаем камеру к текущему местоположению
-                            if (mMap != null) {
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15));
-                            }
-                        }
-                    });
-        } else {
-            // Если разрешение не получено, запрашиваем его у пользователя
-            ActivityCompat.requestPermissions(requireActivity(),
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        }
         return view; // Возвращаем view для отображения фрагмента
     }
 
     // Метод для добавления нового маркера на карту при клике пользователя по карте
     private void addMarker() {
-        if (mMap != null) { // Проверяем, что объект карты не null
-            // Устанавливаем слушатель клика по карте
+        if (mMap != null) {
             mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                 @Override
                 public void onMapClick(@NonNull LatLng latLng) {
@@ -214,7 +206,6 @@ public class MapFragment extends Fragment {
                             .position(latLng)
                             .title(latLng.latitude + ":" + latLng.longitude);
 
-                    // Добавляем маркер на карту и отключаем слушатель клика, чтобы добавить только один маркер
                     Marker marker = mMap.addMarker(markerOptions);
                     mMap.setOnMapClickListener(null); // Отключаем слушатель после добавления маркера
                 }
@@ -241,10 +232,9 @@ public class MapFragment extends Fragment {
     }
 
     private void checkIfUserLocationVisibleAndUpdateButton() {
-        // Проверяем наличие разрешений на доступ к местоположению
         if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return; // Если разрешений нет, выходим из метода
+            return;
         }
 
         // Получаем последнее известное местоположение
@@ -254,13 +244,10 @@ public class MapFragment extends Fragment {
                 // Проверяем, находится ли текущее местоположение в пределах видимой области карты
                 isUserLocationVisible = mMap.getProjection().getVisibleRegion().latLngBounds.contains(currentLatLng);
 
-                // Обновляем внешний вид кнопки местоположения в зависимости от видимости местоположения пользователя
                 Button myLocationButton = getView().findViewById(R.id.MyLocationbtn);
                 if (isUserLocationVisible) {
-                    // Если местоположение видимо, используем один стиль кнопки
                     myLocationButton.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.my_location_visible));
                 } else {
-                    // Если местоположение не видимо, используем другой стиль кнопки
                     myLocationButton.setBackground(ContextCompat.getDrawable(getActivity(), R.drawable.my_location_not_visible));
                 }
             }
