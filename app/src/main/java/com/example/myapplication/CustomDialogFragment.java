1dc3769787b0c238a61f6907a566c35841af8a0f
Tigran <tigmir5@gmail.com>
2024-03-13 17:08:50 +0400

    CustomDialog changes

--- app/src/main/java/com/example/myapplication/CustomDialogFragment.java
+++ app/src/main/java/com/example/myapplication/CustomDialogFragment.java
@@ -2,18 +2,22 @@ package com.example.myapplication;
 
 import android.app.Dialog;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-public class CustomDialogFragment extends DialogFragment {
 
+public class CustomDialogFragment extends DialogFragment {
     private String title;
     private String message;
+    private boolean isFavorited = false;
 
     // Constructor to pass title and message
     public CustomDialogFragment(String title, String message) {
@@ -29,6 +33,7 @@ public class CustomDialogFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.dialog_history, null); // Assuming dialog_history.xml is your custom dialog layout
 
         ImageButton cancelButton = view.findViewById(R.id.cancelID);
+        Button starButton = view.findViewById(R.id.star_button); // The ID of your button
         TextView titleTextView = view.findViewById(R.id.tvAlertTitle);
         TextView messageTextView = view.findViewById(R.id.tvAlertMessage);
 
@@ -45,9 +50,21 @@ public class CustomDialogFragment extends DialogFragment {
 
         builder.setView(view);
         AlertDialog dialog = builder.create();
-
-        // Set the background of the dialog window to null
         dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        starButton.setBackgroundResource(isFavorited ? R.drawable.my_favorite_place : R.drawable.my_not_favorite_place);
+
+        // Set up the click listener to toggle the drawable
+        starButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle the state
+                isFavorited = !isFavorited;
+
+                // Change the button's background drawable based on the toggled state
+                starButton.setBackgroundResource(isFavorited ? R.drawable.my_favorite_place : R.drawable.my_not_favorite_place);
+            }
+        });
 
         return dialog;
     }
